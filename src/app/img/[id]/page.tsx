5cd2f937f8c9f50d4a100e9e3deb9641f181c53a
Colton Bassett <coltonbassett@gmail.com>
2024-09-23 16:59:12 -0700

    Improved dialog and fixed full screen bug, made component for image view

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default function PhotoModal({
+import { getImage } from "~/server/queries";
+import FullPageImageView from "~/components/full-image-page";
+
+export default async function PhotoPage({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
+  return <FullPageImageView id={idAsNumber}></FullPageImageView>;
 }
