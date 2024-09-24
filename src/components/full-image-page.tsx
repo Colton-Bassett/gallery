e57e6952307fe27e6e2e8a569437619b3a5d7ff1
Colton Bassett <coltonbassett@gmail.com>
2024-09-24 14:00:40 -0700

    Continuing to style modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,15 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96"></img>;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="w-96 flex-shrink object-contain"></img>
+      </div>
+
+      <div className="w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
